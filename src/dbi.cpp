b023303e29597a94e93fcf773f5ed80d098a9cad
Kun Wang <wk910930@gmail.com>
2015-06-15 22:41:40 +0800

    add code in dbi

--- src/dbi.cpp
+++ src/dbi.cpp
@@ -1,16 +1,29 @@
-
+//
+// Copyright 2015 CUHK
+//
 
 #include "dbi.hpp"
 
+#include <string>
 
+bool CDbi::Connect(const std::string& user, const std::string& pass, const std::string& url) {
+    try {
+        sql::Driver * driver = sql::mysql::get_driver_instance();
+        /* Using the Driver to create a connection */
+        con_.reset(driver->connect(url, user, pass));
+    } catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+        return false;
+    }
+    return true;
+}
 
-void CDbi::b() {
+bool CDbi::UseDB(const std::string& database) {
+    /* Creating a "simple" statement - "simple" = not a prepared statement */
+    boost::scoped_ptr< sql::Statement > stmt(con_->createStatement());
 
-
-
-
-
-    return;
-
+    /* Create a test table demonstrating the use of sql::Statement.execute() */
+    stmt->execute("USE " + database);
+    return true;
 }
 
