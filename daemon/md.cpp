fc4481accac5cef8947e6ff6e1f3a91bf59075b7
Kun Wang <wk910930@gmail.com>
2015-09-11 12:47:40 +0800

    Remove redundant header files in daemons
    * Since common.hpp includes all header files, it doesn't make sence to include same header files again in cpp files.
    * **WARNING:** common.hpp contains some uncommon header files (e.g. thread, stdlib and etc.), which might make binary files larger than we need.

--- daemon/md.cpp
+++ daemon/md.cpp
@@ -2,24 +2,7 @@
 // Copyright 2015 CUHK
 //
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-
-#include <opencv2/highgui/highgui.hpp>
-
-#include <boost/interprocess/shared_memory_object.hpp>
-#include <boost/interprocess/mapped_region.hpp>
-
-#include <iostream>
-#include <string>
-#include <thread>
 #include "comm.hpp"
-
 #include "task.hpp"
 
 
--- daemon/rd.cpp
+++ daemon/rd.cpp
@@ -2,21 +2,7 @@
 // Copyright 2015 CUHK
 //
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-
-
-
-//#include <iostream>
-//#include <string>
-#include <thread>
 #include "comm.hpp"
-
 #include "task.hpp"
 
 
