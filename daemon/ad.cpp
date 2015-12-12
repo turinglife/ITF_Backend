0ddc531ca638ebc34a9d6eb091bce5c9b228fcc7
Kun Wang <wk910930@gmail.com>
2015-12-12 20:58:45 +0800

    Formatting the cd.cpp and ad.cpp
    * use glog to take place of std::cout
    * code style passed Google C++ Style Guide
    * make code compact and neat

--- daemon/ad.cpp
+++ daemon/ad.cpp
@@ -2,68 +2,47 @@
 // Copyright 2015 CUHK
 //
 
-#include "task.hpp"
 #include "comm.hpp"
-
+#include "task.hpp"
 
 int main(int argc, char* argv[]) {
-    std::string task_name(argv[1]);
-    CHECK(!task_name.empty()) << "task_name cannot be empty";
+  google::InitGoogleLogging(argv[0]);
+  LOG_IF(ERROR, argc < 2) << "task_name is missing!";
+  std::string task_name(argv[1]);
+  CTask<float> task;
+  // initialize analyzer
+  if (!task.InitAnalyzer(task_name))
+    LOG(ERROR) << "initialze analyzer fail!";
+  // setup server
+  std::string socket_path = "AD_" + task_name;
+  CComm server;
+  if (!server.Establish(socket_path))
+    LOG(ERROR) << "Fail to establish connection";
 
-    CTask<float> task;
-    // Init analyzer
-    if (!task.InitAnalyzer(task_name)) {
-        std::cerr << "init analyzer fail" << std::endl;
-        std::cerr << "ad exit" << std::endl;
-        return -1;
+  std::thread worker;
+  while (true) {
+    std::string action;
+    server.Receive(action);
+    if (action.compare("START") == 0) {
+      task.setFuncStatus(CTask<float>::TERMINATE);
+      if (worker.joinable()) worker.join();
+      task.setFuncStatus(CTask<float>::RUNNING);
+      task.setTaskStatus(CTask<float>::ON);
+      worker = std::thread(&CTask<float>::Analyze, &task);
+      server.Reply("OK");
+    } else if (action.compare("STOP") == 0) {
+      task.setFuncStatus(CTask<float>::TERMINATE);
+      task.setTaskStatus(CTask<float>::OFF);
+      if (worker.joinable()) worker.join();
+      server.Reply("OK");
+      break;
     } else {
-        std::cout << "Analyzer is initialized" << std::endl;
+      LOG(WARNING) << action << " is an invalid command!";
+      server.Reply("NO");
     }
-
-    // Server comm;
-    CComm server;
-    std::string socket_path = "AD_" + task_name;
-    if (!server.Establish(socket_path)) {
-        std::cerr << "Fail to establish connection" << std::endl;
-        return -1;
-    }
-
-    std::thread t_work;
-
-    std::cout << "ad is ready" << std::endl;
-
-    while (true) {
-        std::string action;
-        server.Receive(action);
-
-        if (action.compare("START") == 0) {  // START
-                task.setFuncStatus(CTask<float>::TERMINATE);
-                if (t_work.joinable())
-                    t_work.join();
-
-                task.setFuncStatus(CTask<float>::RUNNING);
-                task.setTaskStatus(CTask<float>::ON);
-
-                // Start analyze thread
-                server.Reply("OK");
-                t_work = std::thread(&CTask<float>::Analyze, &task);
-
-        } else if (action.compare("STOP") == 0) {  // STOP
-                task.setFuncStatus(CTask<float>::TERMINATE);
-                task.setTaskStatus(CTask<float>::OFF);
-                if (t_work.joinable())
-                    t_work.join();
-                server.Reply("OK");
-                break;
-        } else {
-            std::cerr << "No such command in ad!" << std::endl;
-            server.Reply("NO");
-        }
-    }
-
-    // only unlink after this process ends
-    unlink(socket_path.c_str());
-
-    std::cout << "ad is done" << std::endl;
-    return 0;
+  }
+  // only unlink after this process ends
+  unlink(socket_path.c_str());
+  std::cout << task_name << ": ad exits successfully!" << std::endl;
+  return 0;
 }
