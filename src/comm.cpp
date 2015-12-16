fc365e564e8971eb6bbf0e322835ecbb01c54740
Kun Wang <wk910930@gmail.com>
2015-12-16 21:46:41 +0800

    Return error info back to client
    * front-end needs the error info to make next move

--- src/comm.cpp
+++ src/comm.cpp
@@ -64,7 +64,7 @@ bool CComm::Send(const std::string& message, const std::string& socket_path, std
     // create a new client
     int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (sockfd < 0) {
-        perror("ERROR socket");
+        received = std::string(strerror(errno));
         return false;
     }
 
@@ -73,18 +73,18 @@ bool CComm::Send(const std::string& message, const std::string& socket_path, std
     snprintf(server_addr.sun_path, socket_path.length() + 1, "%s", socket_path.c_str());
 
     if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-        perror("ERROR connect");
+        received = std::string(strerror(errno));
         return false;
     }
 
     if (write(sockfd, message.c_str(), message.length() + 1) < 0) {
-        perror("ERROR write");
+        received = std::string(strerror(errno));
         return false;
     }
 
     char tmp_msg[256] = {0};
     if (read(sockfd, tmp_msg, 255) <= 0) {
-        perror("ERROR read");
+        received = std::string(strerror(errno));
     } else {
         received = std::string(tmp_msg);
     }
